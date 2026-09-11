96097cb27d90865c93f010b34a9f6237b5f79408
dityans <dityan25@gmail.com>
2026-09-11 08:49:16 +0700

    Tambah Objek Mahasiswa

--- Jobsheet2/MahasiswaDemo.java
+++ Jobsheet2/MahasiswaDemo.java
@@ -7,7 +7,24 @@ public class MahasiswaDemo {
         m1.nama = "Yansy Ayuningtias";
         m1.alamat = "Nias, Sumatera Utara";
         m1.kelas = "2A";
-
         m1.displayBiodata();
+        
+        System.out.println();
+
+        Mahasiswa m2 = new Mahasiswa();
+        m2.nim = "023433";
+        m2.nama = "Budi Santoso";
+        m2.alamat = "Malang, Jawa Timur";
+        m2.kelas = "2A";
+        m2.displayBiodata();
+
+        System.out.println();
+
+        Mahasiswa m3 = new Mahasiswa();
+        m3.nim = "023434";
+        m3.nama = "Siti Rahma";
+        m3.alamat = "Surabaya, Jawa Timur";
+        m3.kelas = "2B";
+        m3.displayBiodata();
     }
 }
