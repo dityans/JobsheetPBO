28f6efe00ae85d0e637b22cb2282d1ae1fd02ef3
dityans <dityan25@gmail.com>
2026-09-18 11:13:45 +0700

    Jobsheet3 Percobaan1

---
+++ Jobsheet3/Motor.java
@@ -0,0 +1,20 @@
+package Jobsheet3;
+
+public class Motor {
+    public String platNomor;
+    public boolean isMesinOn;
+    public int kecepatan;
+
+    public void displayStatus(){
+        System.out.println("Plat Nomor: " + this.platNomor);
+        
+        if (isMesinOn) {
+            System.out.println("Mesin ON");
+        } else {
+            System.out.println("Mesin OFF");
+        }
+
+        System.out.println("Kecepatan: " + this.kecepatan);
+        System.out.println("===========================");
+    }
+}
